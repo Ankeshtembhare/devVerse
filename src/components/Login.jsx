e1f2bf587d5cb6d72eeb0ce25732f612e6dd0549
Ankesh <atankesh219@gmail.com>
2025-06-18 11:20:07 +0530

    user login and redux store

---
+++ src/components/Login.jsx
@@ -0,0 +1,72 @@
+
+ import { useState } from "react";
+ import axios from "axios";
+ import { useDispatch } from "react-redux";
+ import { addUser } from "../utills/userSlice";
+import { useNavigate } from "react-router-dom";
+import { BASE_URL } from "../utills/constants";
+
+const Login = () => {
+  const [emailID, setEmailId] = useState("rohit@gmail.com");
+  const [password, setPassword] = useState("Rohit@123");
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const handleLogin = async() =>{
+
+    try{
+        const  res = await axios.post( BASE_URL+'/login',{
+          emailID,
+          password
+        },{
+          withCredentials:true,
+        });
+        
+        dispatch(addUser(res.data));
+        return navigate("/");
+    }catch(err){
+      console.error(err);
+    }
+
+  
+
+  };
+
+  return (
+    <div className="flex justify-center my-6">
+      <div className="card bg-base-300 w-96 shadow-sm">
+        <div className="card-body">
+          <h2 className="card-title flex justify-center">LOGIN</h2>
+
+          <div className="py-4">
+            <fieldset className="fieldset mb-4">
+              <legend className="fieldset-legend my-2 py-2">EMAIL</legend>
+              <input
+                type="text"
+                value={emailID}
+                className="input"
+                onChange={(e) => setEmailId(e.target.value)}
+              />
+            </fieldset>
+
+            <fieldset className="fieldset mb-4">
+              <legend className="fieldset-legend my-2 py-2">PASSWORD</legend>
+              <input
+                type="password"
+                value={password}
+                className="input"
+                onChange={(e) => setPassword(e.target.value)}
+              />
+            </fieldset>
+          </div>
+
+          <div className="card-actions justify-center py-4">
+            <button className="btn btn-primary" onClick={handleLogin}>Login</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
